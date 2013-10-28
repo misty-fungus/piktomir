a7da00db8a554bf7a726916c2a12aef876e6f434
Yaroslav Leonov <alximik43@gmail.com>
2013-10-28 12:22:13 +0400

    further debianization

--- debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(piktomir):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
-  title="piktomir" command="/usr/bin/piktomir"
--- debian/piktomir.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the piktomir package
-#
-0 4	* * *	root	[ -x /usr/bin/piktomir_maintenance ] && /usr/bin/piktomir_maintenance
